7597298af0191b4d09e44a88e0051f4a23c6a798
mikko <mikko.pek@gmail.com>
2022-03-03 10:02:01 +0200

    changes in codes

--- src/data/queries.py
+++ src/data/queries.py
@@ -1,19 +1,4 @@
-from configparser import ConfigParser
-from logging import exception
 import psycopg2
-
-def config(filename='database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
 
 def connect():
     con = None
@@ -40,5 +25,3 @@ def select_all(cursor):
 
 if __name__ == '__main__':
     connect()
-
-
