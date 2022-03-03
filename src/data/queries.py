b220300ad93ce7d8827f5c432c72ad04c7f7cbd6
mikko <mikko.pek@gmail.com>
2022-03-03 11:00:24 +0200

    changes in code

--- src/data/queries.py
+++ src/data/queries.py
@@ -6,7 +6,9 @@ def connect():
     try:
         con = psycopg2.connect(**config())
         cursor = con.cursor()
-        select_all(cursor)
+        #select_all(cursor)
+        select_all_2(cursor)
+        #column_names(cursor)
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -23,6 +25,27 @@ def select_all(cursor):
     while row is not None:
         print(row)
         row = cursor.fetchone()
+
+def select_all_2(cursor):
+    SQL = 'SELECT * FROM certificates;'
+    cursor.execute(SQL)
+    colnames = [desc[0] for desc in cursor.description]
+    print(colnames)
+    row = cursor.fetchone()
+    while row is not None:
+        print(row)
+        row = cursor.fetchone()
+
+def column_names(cursor):
+    SQL = "SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'person';" 
+    cursor.execute(SQL)
+    colnames = [desc[0] for desc in cursor.description]
+    print(colnames)
+    row = cursor.fetchone()
+    while row is not None:
+        print(row)
+        row = cursor.fetchone()
+
 
 if __name__ == '__main__':
     connect()
